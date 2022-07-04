385afa282b3c055f98e2afbd136d426901aede8b
Jack Young <108631484+JackYoung321@users.noreply.github.com>
2022-07-04 01:16:46 +0100

    Update 20-ValidParentheses.py

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -8,6 +8,7 @@ class Solution:
         
         T = False # Set initial truth checking value to false
         last_char = s[-1] # Get the last value in the sample string to know when to finish checking
+        
         for a in s: # For each character in the string
            
             for i, v in characters.items():  # For i (key), v (value) in the character dictionary
@@ -28,8 +29,7 @@ class Solution:
                             
                         else:               # If it is the last character
                             return True     #The string has been fully checked, with all open brackets being closed
-                        
-                        
+                                             
                 else:               #If the truth value is not set to True (we arent expecting a bracket to close)
 
                     if a == i:      #If the current character matches one of the keys in the dictionary
@@ -38,7 +38,7 @@ class Solution:
                                      #been set as the corresponding closing bracket)
                         T = True     #Set truth checking statement to true
                         break        #Break out the for loop to check the next letter
-                    
+                        
                     #else:
                         #return False # Contains a character that is not accepted - This would be added if the current
                         #constraints did not exist
